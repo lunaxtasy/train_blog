759167556781a82e8d1de47dac0fb2edefe6a355
Becky <55168393+lunaxtasy@users.noreply.github.com>
2020-02-03 22:03:39 -0500

    Assignment 1 Blog project in Django

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,8 +1,12 @@
 """
 test_views.py
+
+Test program for checking views.py
 """
 
 def test_index_ok(client):
-    """checks that HTTP response for the index view is 200"""
+    """
+    checks that HTTP response for the index view is 200
+    """
     response = client.get('/')
     assert response.status_code == 200
--- train_blog/views.py
+++ train_blog/views.py
@@ -1,9 +1,13 @@
 """
 train_blog/views.py
+
+The visible bits
 """
 
 from django.http import HttpResponse
 
 def index(Response):
-    """returns text string to browser when HTTP request is received"""
+    """
+    returns text string to browser when HTTP request is received
+    """
     return HttpResponse("Welcome to British Model Trains Ltd!")
