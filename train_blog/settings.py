667fea7e1a99a00f2de2c6048b4d1014b3b258e5
Becky <55168393+lunaxtasy@users.noreply.github.com>
2020-03-11 00:34:54 -0400

    Assignment 4

--- train_blog/settings.py
+++ train_blog/settings.py
@@ -118,10 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    '/blog/static/',
+    os.path.normpath('C:/Users/inora/projects/train_blog/blog/static'),
+    #'/blog/static',
 ]
