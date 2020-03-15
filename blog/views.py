e26d8348c470c11c4bfd3dd667bb151dd2f0aecf
Becky <55168393+lunaxtasy@users.noreply.github.com>
2020-03-14 23:15:46 -0400

    Actually Assignment 3, working on Assignment 4, figured out why the sidebar lists weren't displaying due to some really bad course notes

--- blog/views.py
+++ blog/views.py
@@ -12,11 +12,13 @@ def home(request):
     """
     #Get last 3 posts
     latest_posts = models.Post.objects.published().order_by('-published')[:3]
+    authors = models.Post.objects.get_authors()
     #Get list of topics
     topics = models.Post.objects.get_topics()
 
     #Add context variable "latest_posts"
     context = {
+        'authors': authors,
         'topics' : topics,
         'latest_posts': latest_posts
     }
